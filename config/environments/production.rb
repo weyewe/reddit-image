024f9712f2d98738a75418fe5dda79cf63311d73
weyewe <w.yunnal@gmail.com>
2013-04-30 20:04:07 +0700

    added precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ AngularCasts::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile = ['*.js', '*.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +47,6 @@ AngularCasts::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
