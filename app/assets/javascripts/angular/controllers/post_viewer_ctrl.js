46bd8172eec43e8441f56cbdab7353cb59fba00f
weyewe <w.yunnal@gmail.com>
2013-04-30 20:19:31 +0700

    fixed the model

--- app/assets/javascripts/angular/controllers/post_viewer_ctrl.js
+++ app/assets/javascripts/angular/controllers/post_viewer_ctrl.js
@@ -2,7 +2,19 @@
 angular.module( 'AngularCasts').controller('PostViewerCtrl', [ '$scope', '$http', '$resource', 
  			function($scope, $http, $resource, $rootScope){
 	$scope.pictureArray = null ;
-	$scope.selectedPost = null  
+	$scope.selectedPost = null  ;
+	
+	
+	$scope.noImages = function(){
+		if( ( $scope.pictureArray === null) || 
+				($scope.pictureArray.length === 0 )
+		){
+			return true;
+		}
+		
+		return false; 
+		
+	}
 	
 	
 	$scope.$on('showPictures', function (event, args) {
--- app/models/g.rb
+++
@@ -1,3 +0,0 @@
-class G < ActiveRecord::Base
-  attr_accessible :duration, :link, :model, :published_at, :screencast, :source,, :summary, :title, :video_url
-end
