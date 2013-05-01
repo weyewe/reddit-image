ff654c25b53f7fd8d3ae486f4ad8336e58e3404d
weyewe <w.yunnal@gmail.com>
2013-05-01 11:04:33 +0700

    fixed the service for minification

--- app/assets/javascripts/angular/services/sub_reddit.js
+++ app/assets/javascripts/angular/services/sub_reddit.js
@@ -1,13 +1,11 @@
-app.factory("SubReddit", function($resource){
+app.factory("SubReddit", ['$resource', function($resource){
 	return $resource("/sub_reddits/:id", {id : "@id"},
-		{
-			'create' : { method : "POST"},
-			'index'  : { method : 'GET', isArray : true },
-			'show'		: {method : 'GET', isArray : false},
-			'update' 	: {method : "PUT"},
-			'destroy' : { method : "DELETE"}
-		}
-	
-	
+	{
+		'create' : { method : "POST"},
+		'index'  : { method : 'GET', isArray : true },
+		'show'		: {method : 'GET', isArray : false},
+		'update' 	: {method : "PUT"},
+		'destroy' : { method : "DELETE"}
+		}]
 	);
 });
